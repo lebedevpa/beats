4816fc7aa3e005448240af942692c14cdc6afc62
lebedevpa <p.lebedev88@gmail.com>
2021-04-03 10:39:08 +0300

    burger menu done

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// hamburger-menu toggle on/off
+
+let button = document.querySelector('#toggle');
+let menu = document.querySelector('#burger-menu');
+let body = document.querySelector('body');
+
+let toggleMenu = function() {
+    button.classList.toggle('hamburger-container--active');
+    menu.classList.toggle('burger-menu--open');
+    body.classList.toggle('body-active-menu');
+
+}
+
+button.addEventListener('click', toggleMenu);   
+
+// hamburger-menu toggle on/off
