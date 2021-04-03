8e7a04ca5c35fcd4f349c8f247310d181ab6ee4c
lebedevpa <p.lebedev88@gmail.com>
2021-04-03 11:40:46 +0300

    animation try

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 
 let button = document.querySelector('#toggle');
 let menu = document.querySelector('#burger-menu');
+let plank = document.querySelector('#plank');
 let body = document.querySelector('body');
 
 let toggleMenu = function() {
     button.classList.toggle('hamburger-container--active');
+    plank.classList.toggle('.hamburger__plank--open');
     menu.classList.toggle('burger-menu--open');
     body.classList.toggle('body-active-menu');
 
