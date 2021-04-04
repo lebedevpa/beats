f10b2d2b740c97011c6d47aa84a7129b0c54a637
lebedevpa <p.lebedev88@gmail.com>
2021-04-04 18:06:35 +0300

    team section done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 // hamburger-menu toggle on/off
 
 let button = document.querySelector('#toggle');
-let menu = document.querySelector('#burger-menu');
-let plank = document.querySelector('#plank');
+let menu = document.querySelector('#burger-menu');  
 let body = document.querySelector('body');
 
 let toggleMenu = function() {
     button.classList.toggle('hamburger-container--active');
-    plank.classList.toggle('.hamburger__plank--open');
     menu.classList.toggle('burger-menu--open');
     body.classList.toggle('body-active-menu');
 
