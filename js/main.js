f3a5b9ae6c5addfe01e0b58c8802691ee5bc2033
lebedevpa <p.lebedev88@gmail.com>
2021-04-20 08:50:39 +0500

    добиваю последние остатки
    слайдер добит
    OPS в процессе

--- js/main.js
+++ js/main.js
@@ -14,5 +14,30 @@ let toggleMenu = function() {
 
 button.addEventListener('click', toggleMenu);   
 
+// OPS
+
+const sections = $("section");
+const display = $(".main-content")
+
+const performTransition = sectionEq => {
+    const position = sectionEq * -10;
+
+    display.css({
+        transform: `translateY(${position}%)`
+    })
+}
+
+$(window).on("wheel", e => {
+    const deltaY = e.originalEvent.deltaY;
+
+    if (deltaY > 0) {
+        performTransition(2);
+
+    }
+
+    if (deltaY < 0) {
 
 
+    }
+
+});
