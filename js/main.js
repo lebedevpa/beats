3d69eb684980ed23dbe4b891e3de9760032fc7a1
lebedevpa <p.lebedev88@gmail.com>
2021-04-20 20:11:10 +0500

    OPS

--- js/main.js
+++ js/main.js
@@ -19,25 +19,85 @@ button.addEventListener('click', toggleMenu);
 const sections = $("section");
 const display = $(".main-content")
 
-const performTransition = sectionEq => {
-    const position = sectionEq * -10;
+let inScroll = false;
 
-    display.css({
-        transform: `translateY(${position}%)`
-    })
-}
+sections.first().addClass("section--active");
+
+const performTransition = sectionEq => {
+
+    if (inScroll === false) {
+        inScroll = true;
+        const position = sectionEq * -100;
+        display.css({
+            transform: `translateY(${position}%)`
+        })
+    
+        sections.eq(sectionEq).addClass("section--active").siblings().removeClass("section--active");
+        
+        setTimeout(() => {
+            inScroll = false;
+        }, 1300);
+    }
+
+   
+};
+
+const scrollViewport = direction => {
+
+    const activeSection = sections.filter(".section--active");
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
+
+    if (direction === "next" && nextSection.length) {
+        performTransition(nextSection.index());
+    }
+
+    if (direction === "prev" && prevSection.length) {
+        performTransition(prevSection.index());
+    }
+};
+
 
 $(window).on("wheel", e => {
     const deltaY = e.originalEvent.deltaY;
 
     if (deltaY > 0) {
-        performTransition(2);
+        scrollViewport("next");
 
     }
 
     if (deltaY < 0) {
-
+        scrollViewport("prev");
 
     }
+
+});
+
+ $(window).on("keydown", (e) => {
+    const tagName = e.target.tagName.toLowerCase();
+
+    if (tagName != "input" && tagName != "textarea") {
+        switch (e.keycode){
+            case 38:
+                scrollViewport("prev");
+                break;
+    
+            case 40:
+                scrollViewport("next");
+                break;
+        }
+    }
+
+
+});
+
+$("[data-scroll-to]").click(e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-scroll-to");
+    const reqSection = $(`[data-section-id=${target}]`);
+
+    performTransition(reqSection.index());
 
 });
